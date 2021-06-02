a060715fb54b61a2c09275a229c6163c87579c40
Giacomo Lanzi <50149567+giacomolanzi@users.noreply.github.com>
2021-06-02 16:58:28 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,16 +7,16 @@
 /**
  * Load child theme css and optional scripts
  * @return void
+ * Remeber to set version number to fixed before productio. Es.: '1.2.3'
  */
 function hello_elementor_child_enqueue_scripts() {
 	wp_enqueue_style(
-		'hello-elementor-child-style',
-		get_stylesheet_directory_uri() . '/style.css',
+		'hello-elementor-child-style', // Name of the style. aka Link Element's ID name
+		get_stylesheet_directory_uri() . '/style.css', // URI
 		[
-			'hello-elementor-theme-style',
+			'hello-elementor-theme-style', // Dependencies
 		],
-		'1.0.0'
+		rand(111,999)
 	);
 }
-
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
